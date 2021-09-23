5084d04e012b28667df9d3473080e6e2cceebcdf
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-24 00:19:29 +0530

    specify types for functions

--- main.hs
+++ main.hs
@@ -4,8 +4,10 @@ safetailpm [] = []
 safetailpm (x:xs) = xs
 
 -- safetail using guarded equations
+safetailg :: [b] -> [b]
 safetailg xs | null xs = []
              | otherwise = tail xs
 
 -- safetail using conditional expressions
+safetailc :: [a] -> [a]
 safetailc xs = if null xs then [] else tail xs
