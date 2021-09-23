9f84438a02edad52a41de71fe00e5f08b60356d2
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-24 00:04:37 +0530

    implement safetail using conditional expressions

--- main.hs
+++ main.hs
@@ -3,6 +3,9 @@ safetailpm::[a] -> [a]
 safetailpm [] = []
 safetailpm (x:xs) = xs
 
--- safetail using guarded expressions
+-- safetail using guarded equations
 safetailg xs | null xs = []
              | otherwise = tail xs
+
+-- safetail using conditional expressions
+safetailc xs = if null xs then [] else tail xs
