fe782837fed7516856cc1d297ef49b75d38ba11f
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-24 00:26:38 +0530

    refefine && using conditionals

--- main.hs
+++ main.hs
@@ -11,3 +11,13 @@ safetailg xs | null xs = []
 -- safetail using conditional expressions
 safetailc :: [a] -> [a]
 safetailc xs = if null xs then [] else tail xs
+
+-----------------------------------------------------------
+
+-- redefine the following version of && using conditionals
+--     True && True = True
+--     _    && _    = False
+and' :: Bool -> Bool -> Bool
+and' x y = if x == True
+            then if y == True then True else False
+            else False
