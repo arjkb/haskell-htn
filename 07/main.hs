19795ea5c425277e398c893bd12ecdc991d15891
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-28 08:56:34 +0530

    define program to find scalar product of 2 lists of integers

--- 07/main.hs
+++ 07/main.hs
@@ -1,3 +1,7 @@
 -- Pythagoreans
 pyths :: Int -> [(Int, Int, Int)]
 pyths n = [ (x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], ((x^2 + y^2) == (z^2))]
+
+-- scalar product
+scalar :: [Int] -> [Int] -> Int
+scalar xs ys = sum [x + y | (x, y) <- zip xs ys]
