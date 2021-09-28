00efdb05699f9013031264c647c3bb363a87c8d0
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-28 08:59:13 +0530

    define helper function to get factors

--- 07/main.hs
+++ 07/main.hs
@@ -5,3 +5,8 @@ pyths n = [ (x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], ((x^2 + y^2) == (
 -- scalar product
 scalar :: [Int] -> [Int] -> Int
 scalar xs ys = sum [x + y | (x, y) <- zip xs ys]
+
+-------------------------------------------------------
+-- perfects
+factors :: Int -> [Int]
+factors n = [x | x <- [2..n-1], (mod n x) == 0]
