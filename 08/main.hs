63ce2c15b8b62e02f2d633fadd5157c305c7507b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-29 23:25:36 +0530

    define concat' using recursion

--- 08/main.hs
+++ 08/main.hs
@@ -4,3 +4,9 @@ and' :: [Bool] -> Bool
 and' (False:_) = False
 and' (True:[]) = True
 and' (True:xs) = and' xs
+
+-- Without looking at prelude, define following using recursion
+-- concat' :: [[a]] -> [a]
+concat' :: [[a]] -> [a]
+concat' (x:[]) = x
+concat' (x:xs) = x ++ (concat' xs)
